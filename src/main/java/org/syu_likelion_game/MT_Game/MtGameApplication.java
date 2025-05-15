8f3198092946f0e785c8675d8e4feca553308669
Gyoseung Ku <106974512+GyoseungKu@users.noreply.github.com>
2025-05-15 19:14:08 +0900

    Add files via upload

---
+++ src/main/java/org/syu_likelion_game/MT_Game/MtGameApplication.java
@@ -0,0 +1,13 @@
+package org.syu_likelion_game.MT_Game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtGameApplication.class, args);
+	}
+
+}
